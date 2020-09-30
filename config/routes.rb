c8064cab07d783eefeee6004e462cf8887ea24df
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-30 19:14:36 -0300

    Created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
+  root to: 'pages#home'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :products, only: [ :index ]
