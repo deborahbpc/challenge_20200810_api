d13b909843025ef6288fa4d10a4678e337fde63d
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-30 18:30:30 -0300

    Created products model. Created api controller

---
+++ db/migrate/20200930212705_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :type_of_product
+      t.text :description
+      t.integer :price
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
