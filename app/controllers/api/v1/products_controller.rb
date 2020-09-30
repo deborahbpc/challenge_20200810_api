3834ccaa099097dbae06550cbf3f86991d782cd3
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-30 19:16:50 -0300

    Fixed scope on controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ProductsController < ApplicationController
     skip_before_action :authenticate_user!, only: :index
 
     def index
-        @products = product_scope(Restaurant)
+        @products = product_scope(Product)
     end
 end
