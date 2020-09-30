16615d4253c50017d8b5d3e082b29dbeac111e40
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-30 19:09:27 -0300

    Ran migration. Fixed route

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
     skip_before_action :authenticate_user!, only: [ :index ]
+    
     def index
         @products = product_scope(Restaurant)
     end
