ab635540948c3ec7f69826a7e0ecde2fc5b695ea
Deborah Carvalho <deborah.bpc@gmail.com>
2020-10-01 19:59:13 -0300

    added some validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
     validates :title, :type, :price, presence: :true
     validates :rating, numericality: { only_integer: true, :greater_than_or_equal_to => 0}, :allow_nil => true
     validates :price, numericality: { only_integer: true, :greater_than => 0, message: "Type an integer greater than 0: last two numbers are cents"}
+    validates :title, uniqueness: true
 
     has_one_attached :photo
 end
